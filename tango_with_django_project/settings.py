52e75ea2caa98c43b66d2b6301302ab5ba39dba6
Callum Neilson <2770691n@student.gla.ac.uk>
2025-02-02 16:03:07 +0000

    chapter 5 tests complete

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -84,9 +84,10 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
